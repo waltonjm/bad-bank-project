b3ef1b56bba787ce707608578fbfeb0fae0bf6f1
Jacqueline Walton <99291782+waltonjm@users.noreply.github.com>
2022-05-03 19:31:04 -0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,11 @@
+function Home(){
+  return (
+    <Card
+      txtcolor="black"
+      header="Solutions for Your Life's Needs"
+      title="Welcome to Bad Bank"
+      text="Go to navigation bar to complete on-line banking transactions."
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+    />    
+  );  
+}
